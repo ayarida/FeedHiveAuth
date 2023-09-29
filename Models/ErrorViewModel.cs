c9f3f635ff111c69bb6fa10d4b7c0cbad504a439
Aya Rida <aya.rida@outlook.com>
2023-09-29 14:56:06 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FeedHiveAuth.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
