7a0887db82fc651effd8fd27042d82df587413b5
Aya Rida <aya.rida@outlook.com>
2023-10-27 14:54:08 +0300

    Context to save global shared vars

---
+++ Services/GlobalContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http.Features;
+using System.Security.Claims;
+
+namespace FeedHiveAuth.Services
+{
+    public class GlobalContext
+    {
+        public HttpContext _globalHttpContext;
+        public string SubscriptionId;
+        public string currentUser;
+
+        public GlobalContext() {
+        }
+
+       
+        public void SetSubscriptionId(string subId)
+        {
+            _globalHttpContext.Session.SetString("subscriptionId",subId);
+        }
+
+        public void SetCurrentUserId(string userId)
+        {
+            //this.currentUser = 
+            //register new value in session
+        }
+
+
+
+    }
+}
