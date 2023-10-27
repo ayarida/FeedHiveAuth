c73d46b5f2d2fffe4ee09703ecc87151ec6eab13
khazzouna <khazzouna@gmail.com>
2023-10-27 11:54:04 +0300

    index page

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+try {
+    const swiper = new Swiper('.swiper', {
+        loop: true,
+        centeredSlides: true,
+        slidesPerView: '1',
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+}
+
+catch (error) {
+    console.log("no swiper found");
+}
+
+
