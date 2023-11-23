afe299ac46442d780da5cee30379f5a475af3ade
khazzouna <khazzouna@gmail.com>
2023-11-23 13:24:13 +0200

    added configs controller and view(still being worked on)

---
+++ Controllers/ConfigsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FeedHiveAuth.Controllers
+{
+    public class ConfigsController : Controller
+    {
+
+        [Area("Social")]
+
+
+        public ActionResult TechincalConfigs()
+        {
+            return View("~/Areas/Social/Views/Channels/Configs/TechincalConfigs.cshtml");
+        }
+    }
+}
