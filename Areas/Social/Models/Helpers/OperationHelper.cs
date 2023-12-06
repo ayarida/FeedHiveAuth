ad5ab11dfe781cb1d75c8394ca8ed31ebe3ea97d
Aya Rida <aya.rida@outlook.com>
2023-12-06 10:24:15 +0200

    Add operation helper and extension for media handling req

---
+++ Areas/Social/Models/Helpers/OperationHelper.cs
@@ -0,0 +1,48 @@
+﻿using FeedHiveAuth.Data.Extensions;
+using FeedHiveAuth.Data;
+using FeedHiveAuth.Models;
+using FeedHiveAuth.Models.Enums;
+
+namespace FeedHiveAuth.Areas.Social.Models.Helpers
+{
+    public static class OperationHelper
+    {
+        public static void StartShareOperation(Operation operation)
+        {
+            operation.StartOperation("The post is being shared to " + operation.Service);
+        }
+
+        public static void EndShareOperation(Operation operation, StatusEnum status, string message = null, string result = null)
+        {
+            operation.EndOperation(status, message, result, "The post share operation to " + operation.Service + " has ended");
+        }
+        private static void StartOperation(this Operation operation, string currentState)
+        {
+            operation.Status = StatusEnum.Processing.Value();
+            operation.CurrentState = currentState;
+            operation.Progress = 0;
+            operation.StartTime = DomainTime.Now();
+            operation.EndTime = null;
+            operation.Messages += "************************************************\r\n" + DomainTime.Now() + ": " + "The operation has started\r\n";
+            Instances.Repositories.OperationRepository.Update(operation);
+        }
+
+        private static void EndOperation(this Operation operation, StatusEnum status, string message = null, string result = null, string currentState = null)
+        {
+            operation.Status = status.Value();
+            operation.CurrentState = currentState;
+            operation.EndTime = DomainTime.Now();
+            if (result.IsNotNullOrEmpty())
+            { //operation.Result = operation.AddResult(result); 
+            }
+            if (message.IsNotNullOrEmpty())
+            { operation.Messages += DomainTime.Now() + ": " + message + "\r\n"; }
+            operation.Messages += DomainTime.Now() + ": " + "The operation has ended\r\n************************************************\r\n";
+            Instances.Repositories.OperationRepository.Update(operation);
+            if (operation.Callback.IsNotNullOrEmpty())
+            {
+                //operation.Callback(status);
+            }
+        }
+    }
+}
---
+++ Data/Extensions/MediaExtension.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+
+namespace FeedHiveAuth.Data.Extensions
+{
+    public static class MediaExtension
+    {
+        public static Stream GetStream(string url)
+        {
+            if(url == null) { return null; }
+            return new FileStream(url, FileMode.Open, FileAccess.Read);
+        }
+        public static Stream GetStreamFromUrl(this string url)
+        {
+            byte[] imageData = null;
+
+            using (var wc = new WebClient())
+                imageData = wc.DownloadData(url.ComposeSafeUrl(url.StartsWith("https:")));
+
+            return new MemoryStream(imageData);
+        }
+    }
+}
