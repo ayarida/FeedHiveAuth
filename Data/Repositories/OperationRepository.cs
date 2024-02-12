fc6e44ecfceab141ea80912bafe98c937177252e
Aya Rida <aya.rida@outlook.com>
2024-02-12 13:42:38 +0200

    initialize operation constructor

--- Data/Repositories/OperationRepository.cs
+++ Data/Repositories/OperationRepository.cs
@@ -4,5 +4,9 @@ namespace FeedHiveAuth.Data.Repositories
 {
     public class OperationRepository : BaseRepository<Operation>
     {
+        public OperationRepository() {
+            Columns = Database.Columns.Operation;
+            TableName = Database.Tables.Operation;
+        }
     }
 }
