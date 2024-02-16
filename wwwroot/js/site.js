6525cd96eb15825c651e21a13592b2cfb47b91db
khazzouna <khazzouna@gmail.com>
2024-02-16 13:29:44 +0200

    fixed single delete in post list

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -46,43 +46,6 @@ function toggle(source) {
     }
 }
 
-function deleteSelected(event) {
-    selected = $("tbody input:checkbox:checked");
-    if (selected.length == 0) {
-
-        $("#empty-delete-error").modal("show");
-
-        $(".close-popup").click(function () {
-            $("#empty-delete-error").modal("hide");
-            location.reload();
-        });
-    }
-    else {
-        var deleteArr = [];
-        selected.each(function (index, value) {
-            deleteArr.push($(value).val());
-        });
-        console.log(deleteArr.join(","));
-        var idsString = deleteArr.join(",");
-        var serviceURL = "/Posts/MultipleDelete";
-        $.ajax({
-            type: "GET",
-            url: serviceURL,
-            data: { idsStr: idsString },
-            success: function (data) {
-                $("#success-message").modal("show");
-                $(".close-popup").click(function () {
-                    $("#success-message").modal("hide");
-                    location.reload();
-                });
-            },
-            error: function (data) {
-                errorFunc();
-            },
-        });
-    }
-
-}
 function deleteMedia(event) {
 
     var serviceURL = "/MediaItem/DeleteMediaItem/" + event;
