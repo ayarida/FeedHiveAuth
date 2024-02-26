8dd34f05eb7429bf6555882d3dc73f674a666fbd
Aya Rida <aya.rida@outlook.com>
2024-02-26 13:06:48 +0200

    Permission helper static class to retrieve all controllers and actions to handle roles permissions

---
+++ Data/Helpers/PermissionHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace FeedHiveAuth.Data.Helpers
+{
+    public static class PermissionHelper
+    {
+        public static Dictionary<string, Tuple<IEnumerable<string>, string>> Controllers()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var controllers = assembly.GetTypes().Where(type => type.IsSubclassOf(typeof(Controller))).ToList();
+
+            return controllers.Select(controller => new 
+            { 
+            Controller = controller, 
+            Actions = Actions(controller)
+            
+            }).ToDictionary(t=>t.Controller.Name.Replace("Controller",""), t=>Tuple.Create(t.Actions,t.Controller.Name));
+        }
+        public static IEnumerable<string> Actions(Type controller)
+        {
+            return controller.GetMethods()
+                .Where(method=>method.IsPublic && method.DeclaringType == controller && !method.IsDefined(typeof(NonActionAttribute)))
+                .Select(method => method.Name).Distinct();
+        }
+    }
+}
