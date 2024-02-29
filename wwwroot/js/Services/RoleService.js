9da24ce043ae5ba79b784eeb7793f558aa706e1a
khazzouna <khazzouna@gmail.com>
2024-02-29 13:37:08 +0200

    send data from create role to controller (style in progress)

---
+++ wwwroot/js/Services/RoleService.js
@@ -0,0 +1,12 @@
+﻿import axios from '~/js/Lib/axios.js'
+
+
+export default class RoleService {
+    static getRoleServices() {
+        
+    }
+    static getRoleService(id) {
+         
+         
+    }
+}
