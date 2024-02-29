ba796391dace894d6c01f049a6c40acd57e377a9
Aya Rida <aya.rida@outlook.com>
2024-02-29 10:39:54 +0200

    Added user view model to limitate all user info needed in views in one model

---
+++ Models/ViewModels/UserDataViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FeedHiveAuth.Models.ViewModels
+{
+    public class UserDataViewModel
+    {
+        public List<Post> userPosts {  get; set; }
+
+        public List<Post> allPosts { get; set; }
+
+        public int userPostsCount { get; set; }
+    }
+}
