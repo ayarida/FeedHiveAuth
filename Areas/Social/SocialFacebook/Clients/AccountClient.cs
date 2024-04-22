b0e17e6ebf7efa3915f0f0b440e03c5913ea0f4e
Aya Rida <aya.rida@outlook.com>
2024-04-22 10:38:14 +0300

    new function for fb pages profile pic (to be used when page thumbnail not show)

--- Areas/Social/SocialFacebook/Clients/AccountClient.cs
+++ Areas/Social/SocialFacebook/Clients/AccountClient.cs
@@ -21,7 +21,27 @@ namespace FeedHiveAuth.Areas.Social.SocialFacebook.Clients
 
             return Get<PictureData>("/picture", parms);
         }
+        public async Task<string> GetProfilePictureUrl(string userId, string accessToken)
+        {
+            string url = $"https://graph.facebook.com/{userId}/picture?access_token={accessToken}";
 
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    // Extract the URL of the profile picture from the response
+                    string profilePictureUrl = response.RequestMessage.RequestUri.ToString();
+                    return profilePictureUrl;
+                }
+                else
+                {
+                    Console.WriteLine($"Failed to fetch profile picture. Status code: {response.StatusCode}");
+                    return null;
+                }
+            }
+        }
         public IRestResponse<User> GetUser(string fields = null)
         {
             var parms = new Dictionary<string, object>();
