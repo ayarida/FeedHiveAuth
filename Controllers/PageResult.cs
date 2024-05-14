80a519e37d3b915d8d3d825fa38cf08194bf931a
khazzouna <khazzouna@gmail.com>
2024-05-14 13:14:02 +0300

    created access dined page and routed to it

---
+++ Controllers/PageResult.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FeedHiveAuth.Controllers
+{
+    public class PageResult : Controller
+    {
+        public IActionResult Forbidden()
+        {
+            return View("~/Views/PageResult/forbidden.cshtml");
+        }
+    }
+}
--- Services/PermissionFilterAttribute.cs
+++ Services/PermissionFilterAttribute.cs
@@ -1,9 +1,7 @@
 ﻿using FeedHiveAuth.Data;
-using FeedHiveAuth.Data.Extensions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
-using System;
 
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
 public class PermissionFilterAttribute : Attribute, IAuthorizationFilter
@@ -13,7 +11,7 @@ public class PermissionFilterAttribute : Attribute, IAuthorizationFilter
     public PermissionFilterAttribute(string permissionName)
     {
         _permissionName = permissionName;
-        
+
     }
 
     public void OnAuthorization(AuthorizationFilterContext context)
@@ -24,21 +22,21 @@ public class PermissionFilterAttribute : Attribute, IAuthorizationFilter
         var userrole = Instances.Repositories.RoleRepository.GetUserRole(currentuser.Id);
         var roleclaims = Instances.Repositories.RoleRepository.GetUserClaims(userrole);
         // Check if the user has the required permission
-        if (!CheckUserPermission(context.HttpContext.User,roleclaims.ToList()))
+        if (!CheckUserPermission(context.HttpContext.User, roleclaims.ToList()))
         {
             // If the user does not have permission, return 403 Forbidden
-            context.Result = new StatusCodeResult(403);
+            context.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "PageResult", action = "Forbidden" }));
         }
     }
 
-    private bool CheckUserPermission(System.Security.Claims.ClaimsPrincipal user,List<string> claims)
+    private bool CheckUserPermission(System.Security.Claims.ClaimsPrincipal user, List<string> claims)
     {
         // Implement your logic to check user's permissions here
         // For example, you can check against a database, roles, etc.
         // In this example, I'm just checking against a hardcoded permission name
-        foreach(var claim in claims)
+        foreach (var claim in claims)
         {
-           if(claim.ToString() ==_permissionName)
+            if (claim.ToString() == _permissionName)
             {
                 return user.HasClaim("Permission", _permissionName);
             }
