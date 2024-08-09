677e4786234bf86834fe8553d2c056c65b15564d
Aya Rida <aya.rida@outlook.com>
2024-08-09 14:30:22 +0300

    BaseController that includes the common function functions and make it the base controller for other controllers

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,52 @@
+﻿using FeedHiveAuth.Areas.Social.Controllers;
+using FeedHiveAuth.Data.Repositories;
+using FeedHiveAuth.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace FeedHiveAuth.Controllers
+{
+    public class BaseController<T> : Controller where T : Controller
+    {
+     
+        protected ChannelRepository _channelService = Instances.Repositories.ChannelRepository;
+        protected UserRepository _userService = Instances.Repositories.UserRepository;
+        private readonly ILogger<T> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
+        public BaseController(UserManager<IdentityUser> userManager, ILogger<T> logger)
+        {
+            _logger = logger;
+            _userManager = userManager;
+        }
+        public async Task<string> identityUserId()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            return userId;
+        }
+        public string currUserId()
+        {
+            return identityUserId().GetAwaiter().GetResult();
+        }
+        public async Task<bool> isAdminAsync()
+        {
+            var currUser = await _userManager.GetUserAsync(User);
+            var isAdmin = currUser != null && await _userManager.IsInRoleAsync(currUser, "Admin");
+            return isAdmin;
+        }
+        public bool isAdmin()
+        {
+            return isAdminAsync().GetAwaiter().GetResult();
+        }
+        public async Task<bool> isMasterAsync()
+        {
+            var currUser = await _userManager.GetUserAsync(User);
+            var isMaster = currUser != null && await _userManager.IsInRoleAsync(currUser, "Master");
+            return isMaster;
+        }
+        public bool isMaster()
+        {
+            return isMasterAsync().GetAwaiter().GetResult();
+        }
+    }
+}
