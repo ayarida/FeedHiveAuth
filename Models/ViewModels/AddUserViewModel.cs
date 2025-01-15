08fb508c7768af77774d02810a10c4ef6f6da02c
Aya Rida <aya.rida@outlook.com>
2025-01-15 12:42:09 +0200

    View Models for user create/tree

--- Models/ViewModels/AddUserViewModel.cs
+++ Models/ViewModels/AddUserViewModel.cs
@@ -4,11 +4,11 @@ namespace FeedHiveAuth.Models.ViewModels
 {
     public class AddUserViewModel
     {
-        public IdentityUser User { get; set; }
+        public ApplicationUser User { get; set; }
         public List<IdentityRole> Roles { get; set; }
-        public List<IdentityUser> Users { get; set; }
+        public List<ApplicationUser> Users { get; set; }
 
-        public IList<IdentityUser> ParentUsers { get; set; }
+        public IList<ApplicationUser> ParentUsers { get; set; }
         public bool IsAdmin { get; set; } = false;
     }
 }
---
+++ Models/ViewModels/SubscriptionViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FeedHiveAuth.Models.ViewModels
+{
+    public class SubscriptionViewModel
+    {
+        public ApplicationUser Master { get; set; }
+
+        public List<ApplicationUser> Admins { get; set; }
+
+        public Dictionary<ApplicationUser, List<ApplicationUser>> Editors { get; set; }
+    }
+}
