b6647506abc97ddea06ccdbbb5c20a22df105267
khazzouna <khazzouna@gmail.com>
2025-11-17 15:21:52 +0200

    changed login page to new design,added logos, added svg icons and ther respective tag helpers and new font

---
+++ CustomTagHelpers/IconTagHelper.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace FeedHiveAuth.CustomTagHelpers
+{
+    [HtmlTargetElement("icon")]
+    public class IconTagHelper : TagHelper
+    {
+        private readonly IWebHostEnvironment _env;
+
+        public IconTagHelper(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        public string Name { get; set; }
+        public int Size { get; set; } = 25;
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            // Correct absolute file path
+            var filePath = Path.Combine(_env.WebRootPath, "Static", "Icons", $"{Name}.svg");
+
+            if (!File.Exists(filePath))
+            {
+                output.Content.SetContent($"<!-- Icon not found: {Name} -->");
+                return;
+            }
+
+            string svg = File.ReadAllText(filePath);
+
+            // Replace width/height dynamically
+            svg = svg.Replace("width=\"25\"", $"width=\"{Size}\"")
+                .Replace("height=\"25\"", $"height=\"{Size}\"");
+
+            output.TagName = null;
+            output.Content.SetHtmlContent(svg);
+        }
+    }
+}
